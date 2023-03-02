dfab8736f51a8b7c5c5b88face955be40b4b35eb
Evan <evan.machefer@ac-dijon.fr>
2023-03-02 21:18:57 +0100

    feat(UI): Modified js to hide blocks on click
    If the title is clicked, hide child divs.

--- org-js.js
+++ org-js.js
@@ -1,3 +1,14 @@
+var h2Color = 'white';
+var h3Color = 'white';
+
+onload = () => {
+    console.log("Loading body");
+    const firstH2 = document.getElementsByTagName("h2")[0];
+    h2Color = firstH2.style.backgroundColor;
+    h3Color = document.getElementsByTagName("h3")[0].style.backgroundColor;
+};
+
+
 onscroll = (event) => {
     var scrolled=window.scrollY;
     var mainTitle = document.getElementsByClassName("title")[0];
@@ -11,16 +22,51 @@ onscroll = (event) => {
     }
 };
 
-onload = () => {
-    console.log("Loading body");  
-};
+
+function isEmpty(node) {
+  return node.textContent.trim() === "";
+}
 
 onclick = (event) => {
     var cible = event.target;
     if (cible.nodeName == 'DIV') {
 	cible.requestFullscreen();
-    } else if (cible.parentNode.nodeName == 'DIV') {
-	// cible.parentNode.requestFullscreen();
-	console.log("parent clicked");
+    } else if (cible.nodeName == 'H2' || cible.nodeName == 'H3') {
+	let inactiveColor = "coral";
+	let activeColor = h2Color;
+	if (cible.nodeName == 'H3') {
+	    activeColor = h3Color;
+	}
+	parent = cible.parentNode;
+	siblings = parent.childNodes;
+	for (let i=0; i < siblings.length; i++) {
+	    if (siblings[i].nodeName == 'DIV') {
+		console.log(i, siblings[i].nodeName)
+		next = siblings[i];
+		if (next.style.display == 'none') {
+		    next.style.display = "block";
+		    cible.style.backgroundColor = activeColor;
+		} else {
+		    next.style.display = "none";
+		    cible.style.backgroundColor = inactiveColor;
+		}
+	    }
+	}
+
+
+	// next = cible.nextElementSibling;
+	// if (isEmpty(next)) {
+	//     next = next.nextElementSibling;
+	// }
+
+	// if (next.style.display == 'none') {
+	//     next.style.display = "block";
+	//     cible.style.backgroundColor = "lightblue";
+	// } else {
+	//     next.style.display = "none";
+	//     cible.style.backgroundColor = "coral";
+	// }
     }
 };
+
+
